39574d243d2c5a4113b8e161a66efa4d4c5b3e9a
vsadauskas <vytautas.sadauskas@gmail.com>
2017-07-31 21:19:34 +0300

    bug fixes and responshito...

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-console.log("testas bliat");
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("manonavig");
@@ -7,5 +5,12 @@ function myFunction() {
         x.className += " responsive";
     } else {
         x.className = "navigacija";
+    }
+    var y = document.getElementById("pic");
+    if (y.className === "navpic"){
+        y.className += " responsive";
+    }
+    else {
+        y.className = "navpic";
     }
 } 
